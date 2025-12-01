bf70141248ed1b5f1d686c02ace5ba508329e72b
janvi-png <sharmajjjanvi@gmail.com>
2025-12-01 18:39:52 +0530

    Add files via upload

---
+++ frontend/components/CompareTab.tsx
@@ -0,0 +1,7 @@
+export default function CompareTab({ plan }) {
+  return (
+    <div className="bg-white/10 p-6 rounded-2xl">
+      <p className="opacity-80">Comparison mode coming next 🎛️</p>
+    </div>
+  );
+}
